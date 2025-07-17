951a436c05fd846123374dafae4bc5c2a4688487
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-07-17 08:18:08 +0200

    Configuracion de CloudWach en AWS

--- api-gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
@@ -3,10 +3,10 @@ package com.example.api_gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
